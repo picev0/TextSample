5e5a8178a1c833f2fcd9d1b366823e92e73b81e3
picev0 <ecotech3690s@yahoo.co.jp>
2020-10-18 16:30:14 +0900

    Initial Commit

---
+++ TextSample/TextSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextSampleApp.swift
+//  TextSample
+//
+//  Created by Shuhei Murata on 2020/10/18.
+//
+
+import SwiftUI
+
+@main
+struct TextSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
