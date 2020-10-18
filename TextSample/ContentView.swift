d03f92b1867cc6823bc3dffbaaaace7b1e965362
picev0 <ecotech3690s@yahoo.co.jp>
2020-10-18 16:55:10 +0900

    Update ContentView.swift

--- TextSample/ContentView.swift
+++ TextSample/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .font(.largeTitle)
+            .foregroundColor(Color.white)
+                .background(Color.gray)
+        
     }
 }
 
